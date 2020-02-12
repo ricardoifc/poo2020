c3f5ec7d7e44dbef9cd59976d8cd7c580fed3342
ricardoifc <Romeo199octavo>
2020-02-12 09:31:09 -0500

    cambios taller

--- poo2020/src/taller1/Celular.java
+++ poo2020/src/taller1/Celular.java
@@ -9,52 +9,52 @@ public class Celular {
     private String color;
     private String sistemaOperativo;
 
-    public String getAlmacenamiento() {
-        return almacenamiento;
-    }
-
-    public String getColor() {
-        return color;
-    }
 
     public String getMarca() {
         return marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public String getMumero() {
-        return mumero;
-    }
-
-    public String getSistemaOperativo() {
-        return sistemaOperativo;
-    }
-
-    public void setAlmacenamiento(String almacenamiento) {
-        this.almacenamiento = almacenamiento;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
+    public String getModelo() {
+        return modelo;
+    }
+
     public void setModelo(String modelo) {
         this.modelo = modelo;
+    }
+
+    public String getMumero() {
+        return mumero;
     }
 
     public void setMumero(String mumero) {
         this.mumero = mumero;
     }
 
+    public String getAlmacenamiento() {
+        return almacenamiento;
+    }
+
+    public void setAlmacenamiento(String almacenamiento) {
+        this.almacenamiento = almacenamiento;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSistemaOperativo() {
+        return sistemaOperativo;
+    }
+
     public void setSistemaOperativo(String sistemaOperativo) {
         this.sistemaOperativo = sistemaOperativo;
     }
-
 }
--- poo2020/src/taller1/Vehiculo.java
+++ poo2020/src/taller1/Vehiculo.java
@@ -1,6 +1,7 @@
 package taller1;
 
 public class Vehiculo {
+
     private String marca;
     private String asientos;
     private String color;
@@ -12,44 +13,44 @@ public class Vehiculo {
         return marca;
     }
 
-    public String getColor() {
-        return color;
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
 
     public String getAsientos() {
         return asientos;
     }
 
-    public String getModelo() {
-        return modelo;
+    public void setAsientos(String asientos) {
+        this.asientos = asientos;
     }
 
-    public String getPlacas() {
-        return placas;
-    }
-
-    public String getTipo_modelo() {
-        return tipo_modelo;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
+    public String getColor() {
+        return color;
     }
 
     public void setColor(String color) {
         this.color = color;
     }
 
-    public void setAsientos(String asientos) {
-        this.asientos = asientos;
+    public String getModelo() {
+        return modelo;
     }
 
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
+    public String getPlacas() {
+        return placas;
+    }
+
     public void setPlacas(String placas) {
         this.placas = placas;
+    }
+
+    public String getTipo_modelo() {
+        return tipo_modelo;
     }
 
     public void setTipo_modelo(String tipo_modelo) {
