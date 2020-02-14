86b1518e07c4d41965e8aea6598bc0d20d52f49f
ricardoifc <Romeo199octavo>
2020-02-14 07:40:48 -0500

    construnctor

--- poo2020/src/deber_seleccion/MayorNumero.java
+++ poo2020/src/deber_seleccion/MayorNumero.java
@@ -1,6 +1,6 @@
 package deber_seleccion;
 
-public class MayorNumero {
+public class  MayorNumero {
     private int numero1;
     private int numero2;
     private int numero3;
@@ -8,41 +8,9 @@ public class MayorNumero {
     private int numMayor;
 
     public MayorNumero (int numero1, int numero2, int numero3, int numero4) {
-        this.setNumero1(numero1);
-        this.setNumero2(numero2);
-        this.setNumero3(numero3);
-        this.setNumero4(numero4);
-    }
-
-    public int getNumero1() {
-        return numero1;
-    }
-
-    public void setNumero1(int numero1) {
         this.numero1 = numero1;
-    }
-
-    public int getNumero2() {
-        return numero2;
-    }
-
-    public void setNumero2(int numero2) {
         this.numero2 = numero2;
-    }
-
-    public int getNumero3() {
-        return numero3;
-    }
-
-    public void setNumero3(int numero3) {
-        this.numero3 = numero3;
-    }
-
-    public int getNumero4() {
-        return numero4;
-    }
-
-    public void setNumero4(int numero4) {
+        this.numero3 = numero3;;
         this.numero4 = numero4;
     }
     public void calcularNumMayor(){
