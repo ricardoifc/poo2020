8688b8b23ae9f43732738e2514fe7097fb935305
ricardoifc <ricardoifc@me.com>
2020-02-20 20:52:36 -0500

    Ejercicio 1

--- src/arreglosdeberpoo/EjecutarFicha.java
+++ src/arreglosdeberpoo/EjecutarFicha.java
@@ -5,25 +5,41 @@ import java.util.Scanner;
 public class EjecutarFicha {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        /**
+         * Declaracion de variables
+         */
         String nombre = "";
-        int edad = 0;
+        String edad = "";
         String universidad = "";
-        String cedula = "";
-        int contador = 1;
-        do{
+        String celular = "";
+        int contador;
+        /**
+         * creacion de objeto Ficha
+         */
+        Ficha ficha = new Ficha();
+        /**
+         * Obtencion de datos mediante un ciclo repetitivo
+         */
+        for(contador = 0; contador <= 4; contador++){
+            System.out.println("Ingrese al estudiante " + contador);
             System.out.println("Ingrese el nombre");
             nombre = sc.nextLine();
             System.out.println("Ingrese la edad");
-            sc.nextInt();
-            edad = sc.nextInt(edad);
+            edad = sc.nextLine();
             System.out.println("Ingrese las iniciales de la universidad");
             universidad = sc.nextLine();
             System.out.println("ingrese la cedula");
-            cedula = sc.nextLine();
+            celular = sc.nextLine();
+            /**
+             * Envio de datos al objeto
+             */
+            ficha.obtenerDatos(nombre, edad, universidad, celular);
+            ficha.almacenarArreglo(contador);
+        }
+        /**
+         * Presentacion de el objeto
+         */
 
-            Ficha ficha = new Ficha(nombre, edad, universidad, cedula);
-
-            contador = contador + 1;
-        }while(contador <= 5);
+        ficha.presentar();
     }
 }
--- src/arreglosdeberpoo/Ficha.java
+++ src/arreglosdeberpoo/Ficha.java
@@ -1,16 +1,50 @@
 package arreglosdeberpoo;
 
 public class Ficha {
+    /**
+     * Declaracion de variables
+     */
     private String nombre;
-    private int edad;
+    private String edad;
     private String universidad;
-    private String cedula;
+    private String celular;
+    private String datosAlumnos[][] = new String[5][4];
 
-    public Ficha(String nombre, int edad, String universidad, String cedula){
+    /**
+     * Metodo constructor para obtencion de datos de estudiante
+     * @param nombre
+     * @param edad
+     * @param universidad
+     * @param celular
+     */
+    public void obtenerDatos(String nombre, String edad, String universidad, String celular){
         this.nombre = nombre;
         this.edad = edad;
         this.universidad = universidad;
-        this.cedula = cedula;
+        this.celular = celular;
     }
 
+    /**
+     * metodo para establecer los datos al array de 2 dimensiones
+     * @param contador
+     */
+    public void almacenarArreglo(int contador){
+        datosAlumnos[contador][0]=nombre;
+        datosAlumnos[contador][1]=edad;
+        datosAlumnos[contador][2]=universidad;
+        datosAlumnos[contador][3]=celular;
+    }
+
+    /**
+     * Metodo para presentar los datos de los estudiatnes
+     */
+    public void presentar() {
+        for (int i = 0; i <=4;i++){
+            System.out.println("\nNombre: "+ datosAlumnos[i][0]);
+            System.out.println("Edad: "+ datosAlumnos[i][1]);
+            System.out.println("Universidad: "+ datosAlumnos[i][2]);
+            System.out.println("Celular: "+ datosAlumnos[i][3] );
+        }
+
+    }
 }
